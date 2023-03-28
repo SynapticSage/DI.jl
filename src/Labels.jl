7960b28a792145aa50a67b03c4a16f18db17e428
Ryan <ryoung@brandeis.edu>
2023-03-28 13:58:21 -0400

    Minor addition to correrr label

--- src/Labels.jl
+++ src/Labels.jl
@@ -1,7 +1,7 @@
 # Labels
 module Labels
 import DataStructures: OrderedDict
-corerr = correct = Dict(0=>"correct", 1=>"error")
+corerr = correct = Dict(0=>"correct", 1=>"error", -1=>"nontask")
 cuemem = tsk = Dict(-1=>"nontask", 0=>"cue",     1=>"mem")
 cortsk = OrderedDict([0,1]=>"CUE correct", [1,1]=>"MEM correct",
                   [0,0]=>"CUE error",   [1,0]=>"MEM error")
