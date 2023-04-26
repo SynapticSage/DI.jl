02d12d8aa496d2e97b73c4f0c9a8b25bf0ccd8ec
Ryan <ryoung@brandeis.edu>
2023-04-26 14:23:29 -0400

    superanimal dataset creatino improvements centered around time .... stagger should also ensure animals are sorted in time and that the time gaps between animals aren't massive (affects downstream plotting)

--- src/superanimal.jl
+++ src/superanimal.jl
@@ -57,20 +57,30 @@ module superanimal
     center_times
 
     centers the times in the stored data per animal/day
+
+    Arguments
+    ---------
+    - numsuperanim::Int=0
+    - data_sources=DI.total_set
+    - append="_cleaned"
+    - stagger_units::Bool=true -- stagger the units, avoid unit collisions
+    - stagger_time::Bool=true -- stagger the times, avoid time collisions
     """
     function center_and_stagger_times_and_neurons(numsuperanim::Int=0; 
-            data_sources=DI.total_set, append="", 
-            stagger_units::Bool=true,stagger_time::Bool=true)
+            data_sources=DI.total_set, append="_cleaned", 
+            stagger_units::Bool=true,
+            stagger_time::Bool=true,
+            if_has_old_unit=:error)
 
         beh, cells = DI.load_behavior("super",numsuperanim) ,
-                     DI.load_cells("super", numsuperanim)
+                     DI.load_cells("super",   numsuperanim)
         time_stats = combine(groupby(beh, [:animal,:day]), 
                            :time=>minimum, :time=>maximum)
 
         time_stats[:,:time_maximum] .= [0; time_stats[1:end-1,:time_maximum]]
         time_stats  = transform(time_stats, 
                               :time_maximum=>cumsum=>:time_prev_maximum)
-        time_stats  = groupby(time_stats, [:animal,:day])
+        time_stats  = groupby(time_stats, [:animal, :day])
 
         unit_stats = combine(groupby(cells, [:animal,:day]), 
                            :unit=>minimum, :unit=>maximum)
@@ -91,19 +101,30 @@ module superanimal
             savemethod = DI.save_functions[source]
             time_vars  = DI.time_vars[source]
             time_vars  = time_vars isa Vector ? time_vars : [time_vars]
-            datum = loadmethod("super", numsuperanim)
+            datum      = loadmethod("super", numsuperanim)
             if source == "cells"
+                if :old_unit in propertynames(datum)
+                    if has_old_unit == :error
+                        @error ":old_unit already in datum -- please check"
+                    elseif has_old_unit  == :continue
+                        @warn ":old_unit already in datum"
+                        continue
+                    else
+                        throw(ValueError("Not a valid option"))
+                    end
+                end
                 datum[!,:old_unit] = datum[:,:unit]
             end
             groups = groupby(datum, [:animal,:day])
-            for key in keys(time_stats)
+            for key in keys(time_stats) # iterate over animal, day
                 mt, nt, group = time_stats[key], 
                             unit_stats[NamedTuple(key)], 
                             groups[NamedTuple(key)]
+                # 
                 for timefield in time_vars
                     group[!,timefield] .-= mt.time_minimum      # center by behavior 0
                     if stagger_time
-                        group[!,timefield] .+= mt.time_prev_maximum # add previous max time of prev dataset
+                        group[!,timefield] .+= mt.time_prev_maximum - mt.time_minimum # add previous max time of prev dataset
                     end
                 end
                 if stagger_units && :unit in propertynames(group)
@@ -112,10 +133,22 @@ module superanimal
                     group.unit .+= nt.unit_prev_maximum # add previous max time of prev dataset
                 end
             end
+            if stagger_time && :time in propertynames(datum)
+                sort!(datum, [:time])
+            else
+                sort!(datum, [x for x in [:animal, :day, :time] 
+                    if x in propertynames(datum)])
+            end
             if source == "cells"
                 @assert datum.old_unit != datum.unit
             end
-            savemethod(datum, "super$append", numsuperanim)
+            if source == "cells"
+                savemethod(datum, "super$append", numsuperanim)
+                savemethod(datum, "super$append", numsuperanim; 
+                           type="arrow")
+            else
+                savemethod(datum, "super$append", numsuperanim)
+            end
         end
         GC.gc()
         @info "be sure to run conversion to arrow if you change anything : tables_to_type()"
@@ -123,4 +156,5 @@ module superanimal
 
     function fix_complex()
     end
+
 end
